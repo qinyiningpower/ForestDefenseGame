75d46c45f55ea5a0a7d957dc725891a6dcc6d2b0
irenetsun88 <qinyining0510@gmail.com>
2026-09-22 11:36:32 +0800

    Update Egg.java

--- src/com/forestdefense/entity/Egg.java
+++ src/com/forestdefense/entity/Egg.java
@@ -1,5 +1,6 @@
 package com.forestdefense.entity;
 
+import javafx.scene.paint.Color;
 import com.forestdefense.base.GameConstant;
 import com.forestdefense.base.GameObject;
 
@@ -17,7 +18,20 @@ public class Egg extends GameObject {
 
     @Override
     public void draw(GraphicsContext gc) {
-        // TODO: UI成员后续绘制鸟蛋
+        gc.setFill(Color.web("#FFF8DD"));
+        gc.fillOval(getX(), getY(), getWidth(), getHeight());
+    
+        gc.setStroke(Color.web("#D8C99E"));
+        gc.setLineWidth(2);
+        gc.strokeOval(getX(), getY(), getWidth(), getHeight());
+    
+        gc.setFill(Color.rgb(255, 255, 255, 0.65));
+        gc.fillOval(
+                getX() + getWidth() * 0.25,
+                getY() + getHeight() * 0.20,
+                getWidth() * 0.20,
+                getHeight() * 0.30
+        );
     }
 
     @Override
